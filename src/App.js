312347b88e90e275a372cdef798b96045260d0fd
TaiwoEnoch <taiwoenoch0@gmail.com>
2024-04-30 18:34:58 +0100

    Change mobile navigation background

--- src/App.js
+++ src/App.js
@@ -5,29 +5,12 @@ import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Service from './Components/Service/Service';
 import Portfolio from './Components/Portfolio/Portfolio';
-// import Blog from './Components/Blog/Blog';
 import Testimonials from './Components/Testimonials/Testimonials';
 import Contact from './Components/Contact/Contact';
-// import Popup from './Components/Portfolio/PopUp/Popup';
-// import Modal from './Components/Modal/subModal';
-
-// import 'animate.css';
-// import AOS from 'aos';
-// import 'aos/dist/aos.css';
 
   
 
 function App() {
-  // useEffect(() => {
-  //   AOS.init({
-  //     duration: 500,
-  //     easing: 'ease-in-out',
-  //   });
-  //   // Refresh AOS whenever the component re-renders
-  //   AOS.refresh();
-  // }, []);
-
-  
   return (
     <Router>
       <div>
