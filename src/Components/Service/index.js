1149b55842e417b36a47f46716008608ae0111f9
TaiwoEnoch <taiwoenoch0@gmail.com>
2024-05-08 15:16:45 +0100

    Added the Service component

---
+++ src/Components/Service/index.js
@@ -0,0 +1,27 @@
+import web from '../constants/websitecont.jpeg';
+import blog from '../constants/pureblog.jpeg';
+
+const service = [
+  {
+    id: 1,
+    image: web,
+    intro: [
+      {
+        title: 'Website Content',
+        desc: 'The jdkgbiudkhiufehduhidhidhiddihdbfbuydbydbydfbydfbydbdbcdbcdbcdbdbdubdududbdcbdudud'
+      }
+    ]
+  },
+  {
+    id: 2,
+    image: blog,
+    intro: [
+      {
+        title: 'Blog Post',
+        desc: 'The jdkgbiudkhiufehduhidhidhiddihdbfbuydbydbydfbydfbydbdbcdbcdbcdbdbdubdududbdcbdudud'
+      }
+    ]
+  }
+];
+
+export default service;
